b6ffb006a91a83a330638b118d9e6ad47edf8ea2
Abhbijit Maity <abhijit.maity8018@gmail.com>
2025-03-15 19:52:35 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// console.log("hii ....")
+// var ans=confirm("Are you an Adult "); // Alert(Message)   Confirm Prompt(User Input)
+// console.log(ans);
+// var a=prompt("Enter your name");
+// console.log(a);
+// function greet(a)
+// {
+//     console.log("Good Morning!",a);
+// }
+// greet("Abhijit Maity");
+//First Class Function
+// var a=function()
+// {
+//     console.log("Hello");
+// }
+// a();
+//Arrow Function
+var a=()=>{
+    console.log("Myself Abhijit");
+}
+a();
